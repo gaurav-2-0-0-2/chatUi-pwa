dd527e6e67c88b5eb596536d261246ebef878f06
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-05-20 16:46:10 +0530

    props added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,10 +3,10 @@ import { FiEdit } from 'react-icons/fi';
 import { FiMoreVertical } from 'react-icons/fi';
 
 
-export default function Navbar() {
+export default function Navbar({start, destination}) {
     return (
         <div>
-            <section className='flex flex-row gap-[16rem]'>
+            <section className='flex flex-row justify-between'>
                 <div className='flex gap-4'>
                     <BsArrowLeft size={30} className='mt-1' />
                     <h1 className='text-2xl font-bold'>Trip 1</h1>
@@ -14,13 +14,13 @@ export default function Navbar() {
                 <FiEdit className='mt-1' size={22} />
             </section>
 
-            <section className='flex gap-[9rem] mt-2'>
+            <section className='flex justify-between mt-2'>
 
                 <div className='flex gap-4'>
                     <img className='w-[50px] h-[50px] rounded-full' src="https://www.pngfind.com/pngs/m/663-6634050_render-anime-jojo-koichi-hirose-png-transparent-png.png" alt="profile" />
                     <div>
-                        <h1>From  <span className='font-bold'>IGI Airport, T3</span></h1>
-                        <h1>To <span className='font-bold'>Sector 28</span></h1>
+                        <h1>From  <span className='font-bold'>{start}</span></h1>
+                        <h1>To <span className='font-bold'>{destination}</span></h1>
                     </div>
 
                 </div>
