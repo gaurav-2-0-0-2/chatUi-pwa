33452801b04ec5fc452866d4d8998dc85a911fe9
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-05-22 11:46:37 +0530

    configured vitePWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
+
+const manifestForPlugin = {
+  registrationType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icons.png", "maskable.png"],
+  manifest: {
+    name: "chatUI-pwa",
+    short_name: "chatUI-pwa",
+    theme_color: "#4ff321",
+    background_color: "#4ff321",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    icons: [
+      {
+        src: "/maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable"
+      },
+      {
+        src: "/logo192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "/logo256.png",
+        sizes: "256x256",
+        type: "image/png"
+      },
+      {
+        src: "/logo384.png",
+        sizes: "384x384",
+        type: "image/png"
+      },
+      {
+        src: "/logo512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
+    ]
+  }
+}
