42230178e14179315a6d100cfdab0f604e3fc2f7
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-05-22 19:15:13 +0530

    vite.config.js modified

--- vite.config.js
+++ vite.config.js
@@ -5,47 +5,56 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), VitePWA(manifestForPlugin)],
+  plugins: [react(), VitePWA(
+    {
+      workbox: {
+           globPatterns: ["**/*"],
+      },
+      includeAssets: ["**/*"],
+      manifest: {
+        name: "chatUI-pwa",
+        short_name: "chatUI-pwa",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon"
+          },
+          {
+            src: "/maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "/logo192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/logo256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/logo384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    }
+  )],
 })
 
-const manifestForPlugin = {
-  registrationType: "prompt",
-  includeAssets: ["favicon.ico", "apple-touch-icons.png", "maskable.png"],
-  manifest: {
-    name: "chatUI-pwa",
-    short_name: "chatUI-pwa",
-    theme_color: "#4ff321",
-    background_color: "#4ff321",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    icons: [
-      {
-        src: "/maskable.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable"
-      },
-      {
-        src: "/logo192.png",
-        sizes: "192x192",
-        type: "image/png"
-      },
-      {
-        src: "/logo256.png",
-        sizes: "256x256",
-        type: "image/png"
-      },
-      {
-        src: "/logo384.png",
-        sizes: "384x384",
-        type: "image/png"
-      },
-      {
-        src: "/logo512.png",
-        sizes: "512x512",
-        type: "image/png"
-      }
-    ]
-  }
-}
